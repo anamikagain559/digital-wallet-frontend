844eaeb7d774d852f985f614e53d3548c173e2fd
anamikagain559 <anamika.zaimahtech@gmail.com>
2025-09-18 11:00:19 +0600

    wallet add

--- src/routes/userSidebarItems.ts
+++ src/routes/userSidebarItems.ts
@@ -1,4 +1,8 @@
-import Bookings from "@/pages/User/Bookings";
+import Deposites from "@/pages/User/Deposits";
+import Profile from "@/pages/User/Profile";
+import Send from "@/pages/User/Send";
+import Transactions from "@/pages/User/Transactions";
+import Withdraw from "@/pages/User/Withdraw";
 import type { ISidebarItem } from "@/types";
 
 export const userSidebarItems: ISidebarItem[] = [
@@ -6,9 +10,29 @@ export const userSidebarItems: ISidebarItem[] = [
     title: "History",
     items: [
       {
-        title: "Bookings",
-        url: "/user/bookings",
-        component: Bookings,
+        title: "Deposits",
+        url: "/user/deposits",
+        component: Deposites,
+      },
+          {
+        title: "Withdraw",
+        url: "/user/withdraw",
+        component: Withdraw,
+      },
+          {
+        title: "Send",
+        url: "/user/send",
+        component: Send,
+      },
+          {
+        title: "Transactions",
+        url: "/user/transactions",
+        component: Transactions,
+      },
+          {
+        title: "Profile",
+        url: "/user/profile",
+        component: Profile,
       },
     ],
   },
